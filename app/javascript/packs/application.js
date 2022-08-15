5fba83c271f9e7fb3e83d068811f193b806e8c81
Fajarullah <frullah12@gmail.com>
2022-08-15 15:01:43 +0700

    use webpacker assets

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,7 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
+console.log("It works!")
